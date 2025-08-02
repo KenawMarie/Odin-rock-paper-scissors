7824e11887fbeec66a40c881fb6f9215b3ccb6c5
kenaw marie <204995551+KenawMarie@users.noreply.github.com>
2025-08-02 21:16:44 +0300

    add pseudocode to main js

---
+++ main.js
@@ -0,0 +1,25 @@
+function getComputerChoice(){
+let r=Math.random()
+if(0<r<=0.33){
+    console.log('paper')
+}
+else if(0.33<r<=0.67){
+    console.log('rock')
+}
+else{
+    console.log('scissors')
+}
+}
+
+
+
+
+
+
+
+
+
+/* 1. Create a new function named getComputerChoice
+   2. Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+   3. 
+*/
