0dd94b89c913cfd282fed9c77bd50130f62a09c1
kenaw marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-09 22:38:35 -0900

    add the function to play until one of the player score five points and add style to it

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const body=document.querySelector('body')
+const container=document.querySelector('.container')
 const hScore=document.querySelector('.hScore');
 const cScore=document.querySelector('.cScore');
 const winner=document.querySelector('.winnerAnnounce')
@@ -15,83 +16,116 @@ function random(num){
 }
 //   ====== get Computer Choice =======
 
-function getComputerChoice(){
-    let choice='Rock Paper Scissors';
-    let randomNumber= Math.floor(Math.random()*3);
-    if(randomNumber === 0){
-        return choice.slice(0,4)
-    }
-    else if(randomNumber === 1){
-        return choice.slice(5,10)
-    }
-    else{
-        return choice.slice(11)
-    }
-
-}
-let choice=getComputerChoice();
 
 // ========= get Human Choice ======
+
+// =============== declare score variables ============
+
+let humanScore = 0;
+let computerScore = 0; 
+
+
 for(const button of buttons){
     button.addEventListener('click',playRound);
 }
 
-    // =============== declare score variables ============
 
-    let humanScore = 0;
-    let computerScore = 0; 
+// ========= the logic to play a single round =========
+
+function playRound(e){
     
-    // ========= the logic to play a single round =========
-
-    function playRound(e){
+    if(humanScore===5 || computerScore===5){
+        if(humanScore>computerScore){
+            container.style.backgroundColor='gold'
+            alert('bravo')
+        }
+        else if(humanScore<computerScore){
+            container.style.backgroundColor='brown'
+            alert('improve for the next time')
+        }
+        else{
+            container.style.backgroundColor='aqua'
+            alert('that is fine')
+        }
+        confirm('do you wanna restart the game')
+        if(confirm){
+            
+        }
+        
+    }
+    console.log(humanScore);
+    console.log(computerScore);
+        function getComputerChoice(){
+            let choice='Rock Paper Scissors';
+            let randomNumber= Math.floor(Math.random()*3);
+            if(randomNumber === 0){
+                return choice.slice(0,4)
+            }
+            else if(randomNumber === 1){
+                return choice.slice(5,10)
+            }
+            else{
+                return choice.slice(11)
+            }
+            
+        }
+        let choice=getComputerChoice();
+        console.log(choice);
         if(e.target.textContent === 'Paper' && choice === 'Rock' ||
             e.target.textContent === 'Rock' && choice === 'Scissors' ||
             e.target.textContent === 'Scissors' && choice === 'Paper')
+            {
+            humanChoice.textContent=`${e.target.previousElementSibling.textContent}`
+            if(choice==='Rock'){
+                computerChoice.textContent=`${buttons[0].previousElementSibling.textContent}`
+            }
+            else if(choice==='Paper'){
+                 computerChoice.textContent=`${buttons[1].previousElementSibling.textContent}`
+                }
+            else if(choice==='Scissors'){
+                 computerChoice.textContent=`${buttons[2].previousElementSibling.textContent}`
+            }
+            humanScore +=1
+            hScore.textContent = humanScore;
+            container.style.backgroundColor='limegreen'
+            winner.textContent=`you won ${e.target.textContent} beats ${choice}`;
+        }
+        
+        else if(e.target.textContent === 'Rock' && choice === 'Paper' ||
+            e.target.textContent === 'Scissors' && choice === 'Rock' ||
+            e.target.textContent === 'Paper' && choice === 'Scissors')
         {
             humanChoice.textContent=`${e.target.previousElementSibling.textContent}`
             if(choice==='Rock'){
                 computerChoice.textContent=`${buttons[0].previousElementSibling.textContent}`
             }
             else if(choice==='Paper'){
-                 computerChoice.textContent=`${buttons[1].previousElementSibling.textContent}`
+                computerChoice.textContent=`${buttons[1].previousElementSibling.textContent}`
             }
             else{
-                 computerChoice.textContent=`${buttons[2].previousElementSibling.textContent}`
+                computerChoice.textContent=`${buttons[2].previousElementSibling.textContent}`
             }
-            hScore.textContent += 1;
-            winner.textContent=`you won ${e.target.textContent} beats ${choice}`;
-        }
-
-        else if(e.target.textContent === 'Rock' && choice === 'Paper' ||
-                e.target.textContent === 'Scissors' && choice === 'Rock' ||
-                e.target.textContent === 'Paper' && choice === 'Scissors')
-        {console.log();
-            humanChoice.textContent=`${e.target.previousElementSibling.textContent}`
-            if(choice==='Rock'){
-                computerChoice.textContent=`${buttons[0].previousElementSibling.textContent}`
-            }
-            else if(choice==='Paper'){
-                 computerChoice.textContent=`${buttons[1].previousElementSibling.textContent}`
-            }
-            else{
-                 computerChoice.textContent=`${buttons[2].previousElementSibling.textContent}`
-            }
-            cScore.textContent +=1;
+            computerScore +=1
+            cScore.textContent =computerScore;
+            container.style.backgroundColor='red'
             winner.textContent=`you lose ${choice} beats ${ e.target.textContent}`;
             
         }
-
+        
         else if(e.target.textContent===choice){
             humanChoice.textContent=`${e.target.previousElementSibling.textContent}`
             computerChoice.textContent=`${e.target.previousElementSibling.textContent}`
             winner.textContent=`it is a tie
             Your choice: ${e.target.textContent}  
             computer choice: ${choice} `;
+            container.style.backgroundColor='skyblue'
         }
-        
-        
-    }
 
+        
+        
+        console.log(hScore.textContent);
+    }
+    
 
 
 
